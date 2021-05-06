61402ce3b6bcdbef44a71e9c10cf8366d366535e
ida <idafalnes@gmail.com>
2021-05-06 19:41:56 +0200

    Add cli utilities

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import stravaAPI_test as test
 import StravaAPI as strava
+import argparse
 
 """
 Source: https://medium.com/swlh/using-python-to-connect-to-stravas-api-and-analyse-your-activities-dummies-guide-5f49727aac86
@@ -9,13 +10,35 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def useCLI(CLI=True, useFlag=1):
+    if CLI is False:
+        return useFlag
+    else:
+        parser = argparse.ArgumentParser(description="Enter task:\n 0: Test\n\n 1: 'Get all data \n\n 2: Test automatic token refresh")
+        parser.add_argument('Task', metavar="Task", type=int,
+                            help="Enter task. Default is 1.")
+        args = parser.parse_args()
+        task = vars(args)['Task']
+        print(task)
+        try:
+            assert task in [0, 1, 2]
+            return task
+        except:
+            print("Need 0, 1 or 2.")
+            print("Running default task (task 1).")
+
+    return useFlag
+
 
 if __name__ == '__main__':
+    useFlag = useCLI(CLI=True)
+    print(useFlag)
+
     MasterFlag = {
         0: '0: Test',
         1: '1: Get all data',
         2: '2: Test automatic token refresh'
-    }[1]
+    }[useFlag]
     if MasterFlag == '0: Test':
         print(MasterFlag)
         test.stravaAuthAPIcall()
